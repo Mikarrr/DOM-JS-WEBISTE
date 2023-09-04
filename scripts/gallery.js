4499a4b4b53e636df7ce5058ded964b6aaf66f16
Mikołaj <karwacki.mikolaj123@gmail.com>
2023-09-04 18:28:26 +0200

    Add gallery anim 2

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -2,7 +2,12 @@ const GalleryListKaspar = document.querySelector(".js-Kaspar");
 
 GalleryListKaspar.addEventListener("mouseenter", () => {
   document.querySelector(".js-photos").style.opacity = "1";
+  document.querySelector(".js-anim-photos").style.transform =
+    "translateX(3000px)";
 });
 GalleryListKaspar.addEventListener("mouseleave", () => {
-  document.querySelector(".js-photos").style.opacity = "0";
+  document.querySelector(".js-anim-photos").style.transform = "translateX(0px)";
+  setTimeout(() => {
+    document.querySelector(".js-photos").style.opacity = "0";
+  }, 1000);
 });
