5ead75b9380ed327c90aad04f7017efd2d97b3d0
Mikołaj <karwacki.mikolaj123@gmail.com>
2023-09-05 11:56:23 +0200

    add 3 version anim gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,13 +1,41 @@
-const GalleryListKaspar = document.querySelector(".js-Kaspar");
+const GalleryList = document.querySelectorAll(".js-gallery-anim li a");
 
-GalleryListKaspar.addEventListener("mouseenter", () => {
-  document.querySelector(".js-photos").style.opacity = "1";
-  document.querySelector(".js-anim-photos").style.transform =
-    "translateX(3000px)";
-});
-GalleryListKaspar.addEventListener("mouseleave", () => {
-  document.querySelector(".js-anim-photos").style.transform = "translateX(0px)";
-  setTimeout(() => {
-    document.querySelector(".js-photos").style.opacity = "0";
-  }, 1000);
+function sliderAnim(element) {
+  element.addEventListener("mouseleave", () => {
+    document.querySelector(".js-anim-photos").style.opacity = "0";
+    setTimeout(() => {
+      document.querySelector(".js-anim-photos").style.transform =
+        "translateX(-4000px)";
+    }, 100);
+
+    setTimeout(() => {
+      document.querySelector(".js-anim-photos").style.opacity = "1";
+      document.querySelector(".js-anim-photos").style.transform =
+        "translateX(0px)";
+      document.querySelector(".js-photos").style.backgroundColor = "none";
+    }, 700);
+  });
+}
+
+GalleryList.forEach((sec) => {
+  const action = sec.getAttribute("data-action");
+
+  sec.addEventListener("mouseenter", () => {
+    if (action === "action1") {
+      document.querySelector(".js-photos").style.backgroundColor = "blue";
+      sliderAnim(sec);
+    }
+    if (action === "action2") {
+      document.querySelector(".js-photos").style.backgroundColor = "red";
+      sliderAnim(sec);
+    }
+    if (action === "action3") {
+      document.querySelector(".js-photos").style.backgroundColor = "green";
+      sliderAnim(sec);
+    }
+
+    document.querySelector(".js-photos").style.opacity = "1";
+    document.querySelector(".js-anim-photos").style.transform =
+      "translateX(3000px)";
+  });
 });
