3f94603e6dca6a78ea833eb45c3e7700dbd83cd6
Mikołaj <karwacki.mikolaj123@gmail.com>
2023-10-11 18:44:13 +0200

    upgrade archives.js

--- scripts/addarchives.js
+++ scripts/addarchives.js
@@ -1,15 +1,17 @@
-let archivesHTML = "";
-
-archives.forEach((archive) => {
-  archivesHTML += `
+const archivesHTML = archives
+  .map(
+    (archive) => `
   <hr />
   <div class="main_content_comming_soon_container">
     <p class="name">${archive.name}</p>
     <p class="data">${archive.data}</p>
     <p class="location">${archive.location}</p>
   </div>
-    `;
-});
-document.querySelector(".js-archives").innerHTML = archivesHTML;
+`
+  )
+  .join("");
+
+const archivesContainer = document.querySelector(".js-archives");
+archivesContainer.innerHTML = archivesHTML;
 
 console.log(archivesHTML);
